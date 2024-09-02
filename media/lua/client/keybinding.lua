0270e810a968cc5d57ec39cf1c06936be229c962
Mário César Melon Bomfim <mariocesarbomfim@gmail.com>
2024-09-02 16:11:11 -0300

    fix

--- media/lua/client/keybinding.lua
+++ media/lua/client/keybinding.lua
@@ -4,13 +4,13 @@ local KEY_SIT = {
 }
 
 if ModOptions and ModOptions.AddKeyBinding then
-	ModOptions:AddKeyBinding("[Hotkeys]",KEY_SIT)
+	ModOptions:AddKeyBinding("[Player Control]",KEY_SIT)
 end
 
 local function KeyUp(keynum)
 	local specificPlayer = getSpecificPlayer(0)
 	if specificPlayer ~= nil then
-		if keynum == KEY_SIT.key then -- Num_0
+		if keynum == KEY_SIT.key then
 			ISWorldObjectContextMenu.onSitOnGround(0)
 	end
 end
