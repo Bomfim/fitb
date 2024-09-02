ca7a305797c3ad5bb9c39e19013096f7ef2dce5e
Mário César Melon Bomfim <mariocesarbomfim@gmail.com>
2024-09-02 16:16:30 -0300

    fix key

--- media/lua/client/keybinding.lua
+++ media/lua/client/keybinding.lua
@@ -8,10 +8,12 @@ if ModOptions and ModOptions.AddKeyBinding then
 end
 
 local function KeyUp(keynum)
+    print ("KeyUp")
 	local specificPlayer = getSpecificPlayer(0)
 	if specificPlayer ~= nil then
 		if keynum == KEY_SIT.key then
 			ISWorldObjectContextMenu.onSitOnGround(0)
+            print ("KEYBACKSLASH")
 	end
 end
 Events.OnKeyPressed.Add(KeyUp);
