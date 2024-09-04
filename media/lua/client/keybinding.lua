9d0a27bc9c0a1d096d6c5eb3f9a07bd4deae893b
Bomfim <mariocesarbomfim@gmail.com>
2024-09-04 01:49:49 -0300

    after a lot strugling, a first version

--- media/lua/client/keybinding.lua
+++ media/lua/client/keybinding.lua
@@ -1,19 +1,43 @@
-local KEY_SIT = {
+require "ISUI/ISPlayerData"
+require "ISUI/ISInventoryPane"
+require "TimedActions/ISTimedActionQueue"
+require "TimedActions/ISInventoryTransferAction"
+
+local KEY_TRANSFER = {
 	name = "Transfer item(s) to backpack",
 	key = Keyboard.KEY_BACKSLASH,
 }
 
 if ModOptions and ModOptions.AddKeyBinding then
-	ModOptions:AddKeyBinding("[Player Control]",KEY_SIT)
+	ModOptions:AddKeyBinding("[Player Control]", KEY_TRANSFER)
+end
+
+--- Extract InventoryItem from Lua table containers
+local function getSingleItem(item)
+    if (item["items"] ~= nil) then
+        return getSingleItem(item["items"])
+    elseif (type(item) == "table") then
+        return getSingleItem(item[1])
+    else
+        return item
+    end
 end
 
 local function KeyUp(keynum)
-    print ("KeyUp")
-	local specificPlayer = getSpecificPlayer(0)
-	if specificPlayer ~= nil then
-		if keynum == KEY_SIT.key then
-			ISWorldObjectContextMenu.onSitOnGround(0)
-            print ("KEYBACKSLASH")
+	if keynum == KEY_TRANSFER.key then
+		local player = getSpecificPlayer(0)
+		local primary = player:getPrimaryHandItem();
+        local secondary = player:getSecondaryHandItem();
+		local selected = getPlayerInventory(0).inventoryPane.selected
+		local back = player:getClothingItem_Back():getItemContainer()
+		for _, inventoryItem in pairs(selected) do
+            local item = getSingleItem(inventoryItem)
+            -- ignore favorite and equipped items
+            if (not item:isFavorite() and not player:isEquipped(item)) then
+                -- create "timed action" to transfer items (game api)
+                ISTimedActionQueue.add(ISInventoryTransferAction:new(player, item, item:getContainer(), back))
+            end
+        end
 	end
 end
 Events.OnKeyPressed.Add(KeyUp);
