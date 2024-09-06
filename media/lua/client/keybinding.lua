5589fdbc49d0b9b525adcfa7664dc031842001ac
Bomfim <mariocesarbomfim@gmail.com>
2024-09-06 00:29:58 -0300

    code adjustments

--- media/lua/client/keybinding.lua
+++ media/lua/client/keybinding.lua
@@ -4,40 +4,30 @@ require "TimedActions/ISTimedActionQueue"
 require "TimedActions/ISInventoryTransferAction"
 
 local KEY_TRANSFER = {
-	name = "Transfer item(s) to backpack",
-	key = Keyboard.KEY_BACKSLASH,
+    name = "Transfer item(s) to backpack",
+    key = Keyboard.KEY_BACKSLASH,
 }
 
 if ModOptions and ModOptions.AddKeyBinding then
-	ModOptions:AddKeyBinding("[Player Control]", KEY_TRANSFER)
-end
-
---- Extract InventoryItem from Lua table containers
-local function getSingleItem(item)
-    if (item["items"] ~= nil) then
-        return getSingleItem(item["items"])
-    elseif (type(item) == "table") then
-        return getSingleItem(item[1])
-    else
-        return item
-    end
+    ModOptions:AddKeyBinding("[Hotkeys]", KEY_TRANSFER)
 end
 
 local function KeyUp(keynum)
-	if keynum == KEY_TRANSFER.key then
-		local player = getSpecificPlayer(0)
-		local primary = player:getPrimaryHandItem();
-        local secondary = player:getSecondaryHandItem();
-		local selected = getPlayerInventory(0).inventoryPane.selected
-		local back = player:getClothingItem_Back():getItemContainer()
-		for _, inventoryItem in pairs(selected) do
-            local item = getSingleItem(inventoryItem)
-            -- ignore favorite and equipped items
-            if (not item:isFavorite() and not player:isEquipped(item)) then
-                -- create "timed action" to transfer items (game api)
-                ISTimedActionQueue.add(ISInventoryTransferAction:new(player, item, item:getContainer(), back))
+    if keynum == KEY_TRANSFER.key then
+        local player = getSpecificPlayer(0)
+        local selectedItems = getPlayerInventory(0).inventoryPane.selected
+        local back = player:getClothingItem_Back():getItemContainer() --getItemContainer()
+
+        if selectedItems == nil then
+            return
+        end
+
+        for _, item in pairs(selectedItems) do
+            local itemsTable = item.items
+            for _, subitem in pairs(itemsTable) do
+                ISTimedActionQueue.add(ISInventoryTransferAction:new(player, subitem, subitem:getContainer(), back))
             end
         end
-	end
+    end
 end
 Events.OnKeyPressed.Add(KeyUp);
