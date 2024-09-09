a987b2d8e1a2abb4ef4ff5494656da9375385234
Bomfim <mariocesarbomfim@gmail.com>
2024-09-09 01:03:19 -0300

    almost the first mod version :)

--- media/lua/client/fitb.lua
+++ media/lua/client/fitb.lua
@@ -1,11 +1,12 @@
 require "ISUI/ISPlayerData"
 require "ISUI/ISInventoryPane"
+require "ISUI/ISInventoryPage"
 require "TimedActions/ISTimedActionQueue"
 require "TimedActions/ISInventoryTransferAction"
 
 local KEY_TRANSFER = {
-    name = "Transfer item(s) to backpack",
-    key = Keyboard.KEY_BACKSLASH,
+    name = "FITB",
+    key = Keyboard.KEY_COMMA,
 }
 
 if ModOptions and ModOptions.AddKeyBinding then
@@ -14,18 +15,30 @@ end
 
 local function KeyUp(keynum)
     if keynum == KEY_TRANSFER.key then
-        local player = getSpecificPlayer(0)
-        local selectedItems = getPlayerInventory(0).inventoryPane.selected
-        local back = player:getClothingItem_Back():getItemContainer() --getItemContainer()
+        local playerInv = getPlayerInventory(0)
+        if playerInv.isCollapsed == true then return; end
 
-        if selectedItems == nil then
-            return
-        end
+        if (table.isempty(playerInv.inventoryPane.selected)) then return; end
+
+        local playerObj = getSpecificPlayer(0)
+        if not playerObj:getClothingItem_Back() or not instanceof(playerObj:getClothingItem_Back(), "InventoryContainer") then return; end
+
+        local selectedItems = playerInv.inventoryPane.selected
+        local backpack = playerObj:getClothingItem_Back():getInventory() --getItemContainer()
+        local hotBar = getPlayerHotbar(0)
+
 
         for _, item in pairs(selectedItems) do
             local itemsTable = item.items
             for _, subitem in pairs(itemsTable) do
-                ISTimedActionQueue.add(ISInventoryTransferAction:new(player, subitem, subitem:getContainer(), back))
+                if backpack:contains(subitem) then return; end
+
+                local ok = not subitem:isFavorite() and not playerObj:isEquipped(subitem) and
+                subitem:getType() ~= "KeyRing" and not hotBar:isInHotbar(subitem)
+                if ok then
+                    ISTimedActionQueue.add(ISInventoryTransferAction:new(playerObj, subitem, subitem:getContainer(),
+                        backpack))
+                end
             end
         end
     end
